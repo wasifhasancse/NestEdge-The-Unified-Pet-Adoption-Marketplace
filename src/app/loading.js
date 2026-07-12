b3c53afd57a0a3b027353c4cadfeccc84c25c81d
Wasif Hasan <wasifhasancse@gmail.com>
2026-07-13 01:48:43 +0600

    Add LoadingPage component to display loading screen with PetLoadingScreen

---
+++ src/app/loading.js
@@ -0,0 +1,12 @@
+import PetLoadingScreen from '@/components/ui/PetLoadingScreen';
+import React from 'react';
+
+const LoadingPage = () => {
+    return (
+        <div>
+            <PetLoadingScreen/>
+        </div>
+    );
+};
+
+export default LoadingPage;
